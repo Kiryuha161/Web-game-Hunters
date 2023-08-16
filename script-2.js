401071c83b63abcc4fb21ce24d7b1110ebc1f4f2
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2023-08-16 22:28:33 +0300

    Есть пока только две свёрстаннные страницы

---
+++ script-2.js
@@ -0,0 +1,5 @@
+let backButtonClick = document.querySelector(".back-button");
+
+backButtonClick.onclick = function () {
+    window.location.href = "Index-1.html";
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+let newGameClick = document.querySelector("#new-game");
+
+newGameClick.onclick = function () {
+    window.location.href = "Index-2.html";
+}
+
+
+
+
+
+
